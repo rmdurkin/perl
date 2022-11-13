386ab314cb72ef8ca2957f408a80edb7256153f7
Robert Durkin <robertmdurkin@gmail.com>
2022-11-13 13:50:23 -0500

    Commit program that prints a multiplication table

---
+++ multitable.pl
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+my $NUM_COLS = 12;
+
+print "\n     ";
+
+my $c = 1;
+while ($c <= $NUM_COLS) {
+    printf("%4d", $c);
+    $c++;
+}
+
+print "\n    +------------------------------------------------\n";
+
+my $r = 1;
+while ($r <= $NUM_COLS) {
+    printf("%4d|", $r);
+    
+    $c = 1;
+    while ($c <= $NUM_COLS) {
+        printf("%4d", $c * $r);
+        $c++;
+    }
+    print "\n";
+    $r++;
+}
